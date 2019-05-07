6d402caeec63e539e2ecaf2bfe91ab69c0840834
daely <dario-e@eye-beyond.com>
2019-05-07 12:12:34 +0300

    added stub implementation for BinaryInputKN and BinaryOutputKN

---
+++ src/mingwMain/kotlin/com.tdunning.math.stats/BinaryInputKN.kt
@@ -0,0 +1,8 @@
+package com.tdunning.math.stats
+
+/**
+ * create a binary input from a input base64 encoded string
+ */
+actual fun buildBinaryInputFromB64(b64string:String):BinaryInput {
+    TODO()
+}
---
+++ src/mingwMain/kotlin/com.tdunning.math.stats/BinaryOutputKN.kt
@@ -0,0 +1,8 @@
+package com.tdunning.math.stats
+
+/**
+ * create a binary output that can be written to
+ */
+actual fun buildBinaryOutput(block: BinaryOutput.() -> Unit): BinaryInput {
+    TODO()
+}
