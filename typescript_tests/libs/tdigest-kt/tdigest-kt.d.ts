1cbfb457e4981d77bb51508f986deb58a9baad94
daely <dario-e@eye-beyond.com>
2019-05-07 15:30:53 +0300

    progress in definition of typescript type definition for tdigest

--- typescript_tests/libs/tdigest-kt/tdigest-kt.d.ts
+++ typescript_tests/libs/tdigest-kt/tdigest-kt.d.ts
@@ -5,15 +5,155 @@
 
 export = tdigest_kt;
 
-declare namespace tdigest {
-    class Centroid {
+declare namespace Kotlin {
+    class Long {
+
+    }
+    interface Collection<T> {
 
     }
 }
+declare namespace tdigesttypes {
+    class Centroid {
+        Companion:Centroid$Companion
+    }
+    class Centroid$Companion {
+    }
+    interface BinaryOutput {
+
+    }
+    interface BinaryInput {
+
+    }
+    interface TDigest {
+        /**
+         * Adds a sample to a histogram
+         *
+         * @param x the sample to add
+         * @param w the weight for the sample: MUST BE AN INTEGER
+         */
+        addWeightedSample(x: number, w: number):void
+        updateSample(oldValue: number, newValue: number):void
+
+        /**
+         * Re-examines a t-digest to determine whether some centroids are redundant.  If your data are
+         * perversely ordered, this may be a good idea.  Even if not, this may save 20% or so in space.
+         *
+         * The cost is roughly the same as adding as many data points as there are centroids.  This
+         * is typically &lt; 10 * compression, but could be as high as 100 * compression.
+         *
+         * This is a destructive operation that is not thread-safe.
+         */
+        compress():void
+
+        /**
+         * Returns the number of points that have been added to this TDigest.
+         *
+         * @return The sum of the weights on all centroids.
+         */
+         size():Kotlin.Long
+
+
+        /**
+         * Returns the fraction of all points added which are  x.
+         *
+         * @param x The cutoff for the cdf.
+         * @return The fraction of all data which is less or equal to x.
+         */
+        cdf(x: number): number
+
+        /**
+         * Returns an estimate of the cutoff such that a specified fraction of the data
+         * added to this TDigest would be less than or equal to the cutoff.
+         *
+         * @param q The desired fraction
+         * @return The value x such that cdf(x) == q
+         */
+        quantile(q: number): number
+
+        /**
+         * A [Collection] that lets you go through the centroids in ascending order by mean.  Centroids
+         * returned will not be re-used, but may or may not share storage with this TDigest.
+         *
+         * @return The centroids in the form of a Collection.
+         */
+        centroids(): Kotlin.Collection<Centroid>
+
+        /**
+         * Returns the current compression factor.
+         *
+         * @return The compression factor originally used to set up the TDigest.
+         */
+        compression(): number
+
+        /**
+         * Returns the number of bytes required to encode this TDigest using #asBytes().
+         *
+         * @return The number of bytes required (an integer)
+         */
+        byteSize(): number
+
+        /**
+         * Returns the number of bytes required to encode this TDigest using #asSmallBytes().
+         *
+         * Note that this is just as expensive as actually compressing the digest. If you don't
+         * care about time, but want to never over-allocate, this is fine. If you care about compression
+         * and speed, you pretty much just have to overallocate by using allocating #byteSize() bytes.
+         *
+         * @return The number of bytes required.(an integer)
+         */
+        smallByteSize(): number
+
+
+        /**
+         * Serialize this TDigest into a byte buffer.  Note that the serialization used is
+         * very straightforward and is considerably larger than strictly necessary.
+         *
+         * @param buf The byte buffer into which the TDigest should be serialized.
+         */
+        asBytes(buf: BinaryOutput):void
+
+        /**
+         * Serialize this TDigest into a byte buffer.  Some simple compression is used
+         * such as using variable byte representation to store the centroid weights and
+         * using delta-encoding on the centroid means so that floats can be reasonably
+         * used to store the centroid means.
+         *
+         * @param buf The byte buffer into which the TDigest should be serialized.
+         */
+        asSmallBytes(buf: BinaryOutput):void
+
+        /**
+         * Add a sample to this TDigest.
+         *
+         * @param x The data value to add
+         */
+        addSample(x: number):void
+
+        /**
+         * Add all of the centroids of another TDigest to this one.
+         *
+         * @param other The other TDigest
+         */
+         addOtherDigest(other: TDigest):void
+
+        centroidCount(): number
+
+
+        Companion:TDigest$Companion
+    }
+    class TDigest$Companion {
+        /**
+         * create a TDigest with the specified compression
+         * use [createAvlTreeDigest] or [createMergingDigest] if you use a specific algorithm
+         * @param compression
+         */
+        createDigest(compression: number):TDigest
+        createMergingDigest(compression: number): TDigest
+        createAvlTreeDigest(compression: number): TDigest
+    }
+}
 declare const tdigest_kt: {
-    $$importsForInline$$: {
-        "tdigest-kt": any;
-    };
     com: {
         tdunning: {
             math: {
@@ -26,7 +166,7 @@ declare const tdigest_kt: {
                     BinaryInputFromByteBuffer: any;
                     BinaryOutput: any;
                     BinaryOutputFromByteBuffer: any;
-                    Centroid: any;
+                    Centroid: tdigesttypes.Centroid;
                     Centroid_init_12fank$: any;
                     Centroid_init_14dthe$: any;
                     Centroid_init_87xbef$: any;
@@ -80,7 +220,7 @@ declare const tdigest_kt: {
                         swap_0: any;
                         swap_1: any;
                     };
-                    TDigest: any;
+                    TDigest: tdigesttypes.TDigest;
                     Utils: {
                         arraycopy_dgpv4k$: any;
                         arraycopy_lvhpry$: any;
--- typescript_tests/tdigest-kt-tests.ts
+++ typescript_tests/tdigest-kt-tests.ts
@@ -1,6 +1,18 @@
 import tdigest_kt = require("tdigest-kt");
 
 var t=tdigest_kt.com.tdunning.math.stats.TDigest;
-var digest = t.Companion.createDigest(200);
+var digest = t.Companion.createAvlTreeDigest(200);
+digest.addSample(10);
+digest.addSample(20);
+digest.addSample(30);
+digest.addSample(40);
+digest.addSample(50);
+digest.addSample(60);
+digest.addSample(70);
+digest.addSample(80);
+digest.addSample(90);
+digest.addSample(100);
+var perc40=digest.quantile(0.5);
+//console.info(perc40)
 var a:number =3;
 var b:number=2;
